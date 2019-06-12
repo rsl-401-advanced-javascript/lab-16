4dc68ebefbba00cdde265e59ca808f2103e71c75
RoryLetteney <rory.letteney@gmail.com>
2019-06-12 13:39:02 -0500

    Changed error handler to break the app and log the trace

--- events/handlers/error.js
+++ events/handlers/error.js
@@ -3,7 +3,9 @@
 const eventHub = require('../event-hub');
 
 
-const errorHandler = err => eventHub.emit('log', err);
+const errorHandler = err => {
+  throw new Error(err);
+}
 
 eventHub.on('error', errorHandler);
 module.exports = errorHandler;
